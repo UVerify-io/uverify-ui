b262364662948be30469dec0b0b16e3ccad774ef
Fabian Bormann <fabian.bormann@cardanofoundation.org>
2025-05-20 17:13:19 +0200

    chore: fix import

--- src/utils/cardano.tsx
+++ src/utils/cardano.tsx
@@ -1,5 +1,5 @@
 import Tag from '../components/Tag';
-import { UVerifyConfig } from './hooks';
+import { UVerifyConfig } from './UVerifyConfigProvider';
 
 type NetworkType = 'mainnet' | 'preprod' | 'preview';
 
