86982bd1780029490bbb034e9d3c005fd53e7a10
Fabian Bormann <fabian.bormann@cardanofoundation.org>
2025-05-20 11:46:42 +0200

    feat: implement tadamon certificate custom ui

--- src/templates/Tadamon/TadamonTemplate.tsx
+++ src/templates/Tadamon/TadamonTemplate.tsx
@@ -12,6 +12,9 @@ import { CircularHash, formatDate } from './utils';
 
 class TadamonTemplate extends Template {
   public name = 'Tadamon';
+  public whitelist = [
+    'addr_test1qpftcj63cky29z6xq69hm454c4ru0tyq89aqcm5kd65wzsevvxgywp50vfnt0raqf0p6y9rq07y4rsrc4fu3k528rc0q8gvagn',
+  ];
 
   public theme = {
     background: 'bg-[#1f242b]',
@@ -44,61 +47,46 @@ class TadamonTemplate extends Template {
       : '';
 
     return (
-      <div className="rounded-[40px] px-12 bg-[#eeeff2] font-['Manrope',sans-serif] text-[#1f242b] flex w-3/4">
-        <div className="grow py-12">
-          <div className="flex flex-col items-start justify-between min-h-100">
-            <img src={tadamonLogo} alt="Tadamon Logo" width={300} />
-            <div className="flex flex-col items-start justify-center mt-16">
-              <p className="text-xl text-[#4E5661]">
-                {formatDate(extra.firstDateTime)}
-              </p>
-              <h1 className="text-5xl text-[##1F242B] font-bold mt-4">
-                {csoName}
-              </h1>
-              <p className="text-xl text-[#4E5661] mt-4">has been awarded a</p>
-              <h1 className="text-4xl text-[##1F242B] font-bold mt-4">
-                CSO Certificate
-              </h1>
-            </div>
-
-            <div className="flex items-start justify-center mt-16">
-              <div className="flex flex-col items-start justify-center mr-8">
-                <h3 className="text-md font-bold mb-1">
-                  {formatDate(extra.firstDateTime)}
-                </h3>
-                <p className="text-xs mb-4">UNDP Sign Date</p>
-                <p className="text-xs">{registrationCountry}</p>
-              </div>
-              <div className="flex flex-col items-start justify-center">
-                <h3 className="text-md font-bold mb-1">
-                  {formatDate(extra.firstDateTime)}
-                </h3>
-                <p className="text-xs mb-4">Beneficiary Sign Date</p>
-                <p className="text-xs">
-                  Certificate Hash: <CircularHash hash={hash} />
-                </p>
-              </div>
-            </div>
+      <div className="rounded-[25px] sm:rounded-[40px] pt-12 px-12 bg-[#eeeff2] font-['Manrope',sans-serif] text-[#1f242b] w-10/12 md:w-3/4 max-w-[800px] mb-12 md:mb-0">
+        <div className="flex flex-col sm:flex-row items-center">
+          <div className="flex w-full sm:w-2/3 justify-center sm:justify-start">
+            <img src={tadamonLogo} alt="Tadamon Logo" width={200} />
           </div>
-        </div>
-        <div className="flex flex-col justify-between items-center relative grow py-12">
-          <div className="flex items-start">
+          <div className="flex w-[200px] sm:w-1/3 mt-4 sm:mt-0 items-center justify-between sm:justify-around">
             <img src={isfdLogo} alt="ISFD Logo" className="h-12 mx-2" />
             <img src={IsDBLogo} alt="IsDB Logo" className="h-12 mx-2" />
             <img src={undpLogo} alt="UNDP Logo" className="h-12 mx-2" />
           </div>
-          <div className="flex flex-col items-center justify-center">
+        </div>
+
+        <div className="flex flex-col md:flex-row items-center md:items-start">
+          <div className="flex w-[200px] md:w-2/3 flex-col items-center sm:items-start justify-between">
+            <div className="flex flex-col items-start justify-center mt-8 sm:mt-16">
+              <p className="text-lg text-[#4E5661]">
+                {formatDate(extra.firstDateTime)}
+              </p>
+              <h1 className="text-4xl text-[##1F242B] font-bold mt-1">
+                {csoName}
+              </h1>
+              <p className="text-lg text-[#4E5661] mt-4">has been awarded a</p>
+              <h1 className="text-3xl font-semibold text-[##1F242B] mt-1">
+                CSO Certificate
+              </h1>
+            </div>
+          </div>
+
+          <div className="flex w-full md:w-1/3 mt-8 flex-col justify-between relative grow">
             <img
               src={veridianCertificateLogo}
               alt="Veridian Certificate Logo"
-              className="h-50 mt-8 mb-2"
+              className="h-50 mb-2"
             />
             <a
               href={`https://${explorerUrlPrefix}cexplorer.io/tx/${
                 certificate!.transaction_hash
               }`}
               target="_blank"
-              className="text-center inline-flex items-center rounded-xl bg-[#1D4D49] px-9 py-3 text-[#F9FAFB] text-sm"
+              className="text-center w-full inline-flex justify-center items-center rounded-xl bg-[#1D4D49] py-3 text-[#F9FAFB] text-xs"
             >
               View on Block Explorer
               <svg
@@ -118,12 +106,50 @@ class TadamonTemplate extends Template {
               </svg>
             </a>
           </div>
-          <div className="flex grow"></div>
-          <img
-            src={plantsLogo}
-            alt="Plants Logo"
-            className="absolute bottom-0 w-3/5 max-w-[200px] transform scale-x-[-1]"
-          />
+        </div>
+
+        <div className="flex flex-col justify-center">
+          <div className="flex w-full sm:w-2/3 mt-12">
+            <div className="flex sm:grow flex-col md:flex-row items-start">
+              <div className="flex flex-col sm:grow">
+                <h3 className="text-md font-semibold mb-1">
+                  {formatDate(extra.firstDateTime)}
+                </h3>
+                <p className="text-xs mb-4 sm:mb-8 font-medium">
+                  UNDP Sign Date
+                </p>
+              </div>
+              <div className="flex flex-col sm:grow">
+                <h3 className="text-md font-semibold mb-1">
+                  {formatDate(extra.firstDateTime)}
+                </h3>
+                <p className="text-xs mb-4 sm:mb-8 font-medium">
+                  Beneficiary Sign Date
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="flex w-full sm:w-2/3 mb-12">
+            <div className="flex flex-col md:flex-row sm:grow">
+              <p className="text-xs sm:grow mt-4 md:mt-0">
+                {registrationCountry}
+              </p>
+              <p className="text-xs sm:grow mt-4 md:mt-0">
+                Certificate Hash: <CircularHash hash={hash} />
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-row items-center">
+          <div className="flex w-2/3 hidden md:block"></div>
+          <div className="flex w-full md:w-1/3 items-center justify-center">
+            <img
+              src={plantsLogo}
+              alt="Plants Logo"
+              className="md:absolute md:bottom-0 md:w-5/7 max-w-[200px] transform scale-x-[-1]"
+            />
+          </div>
         </div>
       </div>
     );
